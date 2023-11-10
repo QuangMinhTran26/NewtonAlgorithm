f9631e0060913f96f7494d380b4dc2808669c2ae
Absolutiom <minh.sthk@gmail.com>
2023-11-10 21:00:31 +0100

    FEAT: add calculateValue

--- src/NewtonAlgorithm.java
+++ src/NewtonAlgorithm.java
@@ -1,7 +1,19 @@
 public class NewtonAlgorithm  {
     private Function function;
+
+    public Function getFunction() {
+        return function;
+    }
+
+    public void setFunction(Function function) {
+        this.function = function;
+    }
+
     public double findRoot(Function function, double x){
-        return 0;
+
+        x = x - function.calculateValue(x)/function.calculateDerivativeAtPoint(x);
+
+        return x;
     }
 
 }
--- src/Polynom.java
+++ src/Polynom.java
@@ -1,9 +1,22 @@
 public class Polynom implements Function {
     private double[] factors;
 
+    public double[] getFactors(){
+        return factors;
+    }
+
+    public void setFactors(double[] factors){
+        this.factors = factors;
+    }
+
     @Override
     public double calculateValue(double x) {
-        return 0;
+        int n = factors.length;
+        double result = 0;
+        for (int i = 0; i < n; i++){
+            result += factors[i]*Math.pow(x,n-i-1);
+        }
+        return result;
     }
 
     @Override
