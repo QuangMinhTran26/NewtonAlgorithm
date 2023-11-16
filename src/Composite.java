d62b61361ac4216d1b889f57141694fe34431ef4
Absolutiom <minh.sthk@gmail.com>
2023-11-16 01:04:35 +0100

    FEAT: add a composite derivative function

--- src/Composite.java
+++ src/Composite.java
@@ -1,3 +1,11 @@
 public class Composite {
 
+    public double composite(double[] outside, double[] inside, double x) {
+        Function in = new Polynomial(inside);
+        Function out = new Polynomial(outside);
+        double resultInside = in.calculateDerivativeAtPoint(x);
+        double resultOutside = 0;
+        resultOutside = out.calculateDerivativeAtPoint(x) * resultInside;
+        return resultOutside;
+    }
 }
